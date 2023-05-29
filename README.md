ee7027401f4ba6603332cd07cc5b0a77b7fe0c02
Nho-way <93555421+Nho-way@users.noreply.github.com>
2023-05-28 21:04:08 -0700

    finished

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Lab 8 - Starter
+1. Within a Github action that runs whenever code is pushed
+In this way, code is automatically tested everytime you push. You don't need to manually run tests every single time. You can simply automate it in Github Actions. Furthermore, you run tests every time code is pushed to make sure we stop bugs before they become too big. We make sure we are pushing solid code. 
+
+
+2. No
+
+3. No; messaging someone is a feature with lots of moving parts. Unit testing was made to debug on a small scale without a lot of moving parts.
+
+4. Yes; the goal is more specific: is the message more or less than 80 characters. We are looking at a very small scale feature with maybe one or two moving parts, which is the point of unit tests.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,78 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+//PHONE NUMBER
+test('Phone Number: (702) 541-0355', () => {
+    expect(functions.isPhoneNumber("(702) 541-0355")).toBe(true);
+  });
+
+test('Phone Number: (702)410355', () => {
+    expect(functions.isPhoneNumber("(702)410355")).toBe(false);
+  });
+
+test('Phone Number: 702-541-0355', () => {
+    expect(functions.isPhoneNumber("702-541-0355")).toBe(true);
+  });
+
+test('Phone Number: 7025410355', () => {
+    expect(functions.isPhoneNumber("7025410355")).toBe(false);
+  });
+
+//EMAIL
+test('Email: niho@ucsd.edu', () => {
+    expect(functions.isEmail("Niho@ucsd.edu")).toBe(true);
+  });
+test('Email: niho@ucsd', () => {
+    expect(functions.isEmail("Niho@ucsd")).toBe(false);
+  });
+test('Email: @ucsd.edu', () => {
+    expect(functions.isEmail("@ucsd.edu")).toBe(false);
+  });
+test('Email: ni_ho@ucsd.edu', () => {
+    expect(functions.isEmail("Ni_ho@ucsd.edu")).toBe(true);
+  });
+
+//StrongPassword
+test('Password: Ni_houcsdedu', () => {
+    expect(functions.isStrongPassword("Ni_houcsdedu")).toBe(true);
+  });
+
+test('Password: Ni_ho@ucsdedu', () => {
+    expect(functions.isStrongPassword("Ni_ho@ucsdedu")).toBe(false);
+  });
+
+test('Password: 7025410355', () => {
+    expect(functions.isStrongPassword("7025410355")).toBe(false);
+  });
+
+test('Password: abcdefg', () => {
+    expect(functions.isStrongPassword("abcdefg")).toBe(true);
+  });
+
+//isDate
+test('Date: 99/99/9999', () => {
+    expect(functions.isDate("99/99/9999")).toBe(true);
+  });
+test('Date: 0/0/0000', () => {
+    expect(functions.isDate("0/0/0000")).toBe(true);
+  });
+test('Date: 1111/1111/11', () => {
+    expect(functions.isDate("1111/1111/11")).toBe(false);
+  });
+test('Date: 22/1111/2', () => {
+    expect(functions.isDate("22/1111/2")).toBe(false);
+  });
+
+//isHexColor
+test('HexColor: FF9FF9', () => {
+    expect(functions.isHexColor("FF9FF9")).toBe(true);
+  });
+test('HexColor: faf', () => {
+    expect(functions.isHexColor("faf")).toBe(true);
+  });
+test('HexColor: ######', () => {
+    expect(functions.isHexColor("######")).toBe(false);
+  });
+test('HexColor: 99', () => {
+    expect(functions.isHexColor("99")).toBe(false);
+  });
